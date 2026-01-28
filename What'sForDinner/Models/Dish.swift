8125d7131d48197cbee71a31f57df4b237770b91
ThibaultGiraudon <thibault.giraudon@gmail.com>
2026-01-28 21:15:27 +0100

    Remove assert causing crash when deleting dishes

--- What'sForDinner/Models/Dish.swift
+++ What'sForDinner/Models/Dish.swift
@@ -10,17 +10,14 @@ import CoreData
 
 class Dish: NSManagedObject {
     var nameValue: String {
-        assert(name != nil, "Dish.name should note be nil")
         return name ?? ""
     }
     
     var ingredientsValue: [Ingredient] {
-        assert(ingredients != nil, "Dish.ingredients should note be nil")
         return (ingredients as? Set<Ingredient>)?.sorted(by: { $0.nameValue < $1.nameValue }) ?? []
     }
     
     var categoriesValue: [Category] {
-        assert(categories != nil, "Dish.categories should note be nil")
         return (categories as? Set<Category>)?.sorted(by: { $0.nameValue < $1.nameValue }) ?? []
     }
 }
